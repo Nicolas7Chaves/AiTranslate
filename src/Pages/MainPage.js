cc4ca6cc6ff1e7d4057a5dbdc364799afc169820
Nicolas7Chaves <nicolas7chaves@gmail.com>
2024-03-06 11:55:51 -0500

    add form for translate submit

--- src/Pages/MainPage.js
+++ src/Pages/MainPage.js
@@ -2,10 +2,18 @@ import './MainPage.scss'
 
 function MainPage() {
 
-    return(
-        <>
-        
-        </>
+    return (
+        <section className='translator'>
+            <div className='translate'>
+                <h2 className='translate__title'>
+                    Text to translate
+                </h2>
+                <form className='translate__form' action='submit' method='post'>
+                    <input type='text' id='translateText' name='Text to Translate' />
+                    <button type='submit'>Translate</button>
+                </form>
+            </div>
+        </section>
     )
 }
 
